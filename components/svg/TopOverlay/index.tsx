b9567fc02c843822501d73beef9a86640cf6f6e2
aravindasiva <aravinda.siva@pego.co.uk>
2021-11-15 18:18:58 +0000

    added header and other things

---
+++ components/svg/TopOverlay/index.tsx
@@ -0,0 +1,199 @@
+import { Box } from "@chakra-ui/layout"
+import * as React from "react"
+
+function TopOverlay(props) {
+  return (
+    <Box {...props}>
+      <svg
+        width={468}
+        height={304}
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        {...props}
+      >
+        <g opacity={0.91} filter="url(#prefix__filter0_d_7_121)">
+          <path
+            d="M291.788-128.658C170.465-234.951 88.184-258.951 35.42-255.151c-52.728 3.797-76.251 35.379-80.02 40.968-.283.42-.395.862-.39 1.467l1.908 205.18c.693 74.511 32.265 143.954 80.66 187.255 48.368 43.274 113.515 60.435 181.562 30.519 70.079-30.809 134.066-80.891 161.919-162.245 9.793-28.604 10.328-52.822 5.364-73.328-4.966-20.512-15.451-37.375-27.798-51.233-12.351-13.861-26.549-24.699-38.914-33.159-4.845-3.315-9.399-6.258-13.454-8.879-1.121-.725-2.205-1.426-3.246-2.102-4.779-3.107-8.701-5.741-11.223-7.95z"
+            stroke="#F1BD70"
+            strokeWidth={2}
+          />
+        </g>
+        <mask
+          id="prefix__a"
+          style={{
+            maskType: "alpha",
+          }}
+          maskUnits="userSpaceOnUse"
+          x={-46}
+          y={-316}
+          width={362}
+          height={391}
+        >
+          <path
+            opacity={0.69}
+            d="M234.682-210.213C34.486-385.43-38.268-291.515-45.407-281.084c-.427.624-.59 1.305-.582 2.061l1.153 123.901C-43.445-5.574 87.604 129.65 214.673 50.783c41.479-25.745 76.61-62.187 94.282-113.752 32.668-95.323-57.965-132.971-74.273-147.244z"
+            fill="#25356A"
+            fillOpacity={0.35}
+          />
+        </mask>
+        <g mask="url(#prefix__a)" fill="#25356A" fillOpacity={0.35}>
+          <circle cx={6.86} cy={0.776} r={5.072} />
+          <circle cx={23.767} cy={0.776} r={5.072} />
+          <circle cx={40.674} cy={0.776} r={5.072} />
+          <circle cx={57.581} cy={0.776} r={5.072} />
+          <circle cx={74.487} cy={0.776} r={5.072} />
+          <circle cx={91.394} cy={0.776} r={5.072} />
+          <circle cx={108.301} cy={0.776} r={5.072} />
+          <circle cx={125.208} cy={0.776} r={5.072} />
+          <circle cx={142.114} cy={0.776} r={5.072} />
+          <circle cx={159.021} cy={0.776} r={5.072} />
+          <circle cx={175.928} cy={0.776} r={5.072} />
+          <circle cx={192.835} cy={0.776} r={5.072} />
+          <circle cx={209.742} cy={0.776} r={5.072} />
+          <circle cx={226.648} cy={0.776} r={5.072} />
+          <circle cx={243.555} cy={0.776} r={5.072} />
+          <circle cx={260.462} cy={0.776} r={5.072} />
+          <circle cx={277.369} cy={0.776} r={5.072} />
+          <circle cx={294.275} cy={0.776} r={5.072} />
+          <circle cx={311.182} cy={0.776} r={5.072} />
+          <circle cx={6.86} cy={17.683} r={5.072} />
+          <circle cx={23.767} cy={17.683} r={5.072} />
+          <circle cx={40.674} cy={17.683} r={5.072} />
+          <circle cx={57.581} cy={17.683} r={5.072} />
+          <circle cx={74.487} cy={17.683} r={5.072} />
+          <circle cx={91.394} cy={17.683} r={5.072} />
+          <circle cx={108.301} cy={17.683} r={5.072} />
+          <circle cx={125.208} cy={17.683} r={5.072} />
+          <circle cx={142.114} cy={17.683} r={5.072} />
+          <circle cx={159.021} cy={17.683} r={5.072} />
+          <circle cx={175.928} cy={17.683} r={5.072} />
+          <circle cx={192.835} cy={17.683} r={5.072} />
+          <circle cx={209.742} cy={17.683} r={5.072} />
+          <circle cx={226.648} cy={17.683} r={5.072} />
+          <circle cx={243.555} cy={17.683} r={5.072} />
+          <circle cx={260.462} cy={17.683} r={5.072} />
+          <circle cx={277.369} cy={17.683} r={5.072} />
+          <circle cx={294.275} cy={17.683} r={5.072} />
+          <circle cx={311.182} cy={17.683} r={5.072} />
+          <circle cx={6.86} cy={34.589} r={5.072} />
+          <circle cx={23.767} cy={34.589} r={5.072} />
+          <circle cx={40.674} cy={34.589} r={5.072} />
+          <circle cx={57.581} cy={34.589} r={5.072} />
+          <circle cx={74.487} cy={34.589} r={5.072} />
+          <circle cx={91.394} cy={34.589} r={5.072} />
+          <circle cx={108.301} cy={34.589} r={5.072} />
+          <circle cx={125.208} cy={34.589} r={5.072} />
+          <circle cx={142.114} cy={34.589} r={5.072} />
+          <circle cx={159.021} cy={34.589} r={5.072} />
+          <circle cx={175.928} cy={34.589} r={5.072} />
+          <circle cx={192.835} cy={34.589} r={5.072} />
+          <circle cx={209.742} cy={34.589} r={5.072} />
+          <circle cx={226.648} cy={34.589} r={5.072} />
+          <circle cx={243.555} cy={34.589} r={5.072} />
+          <circle cx={260.462} cy={34.589} r={5.072} />
+          <circle cx={277.369} cy={34.589} r={5.072} />
+          <circle cx={294.275} cy={34.589} r={5.072} />
+          <circle cx={311.182} cy={34.589} r={5.072} />
+          <circle cx={6.86} cy={51.496} r={5.072} />
+          <circle cx={23.767} cy={51.496} r={5.072} />
+          <circle cx={40.674} cy={51.496} r={5.072} />
+          <circle cx={57.581} cy={51.496} r={5.072} />
+          <circle cx={74.487} cy={51.496} r={5.072} />
+          <circle cx={91.394} cy={51.496} r={5.072} />
+          <circle cx={108.301} cy={51.496} r={5.072} />
+          <circle cx={125.208} cy={51.496} r={5.072} />
+          <circle cx={142.114} cy={51.496} r={5.072} />
+          <circle cx={159.021} cy={51.496} r={5.072} />
+          <circle cx={175.928} cy={51.496} r={5.072} />
+          <circle cx={192.835} cy={51.496} r={5.072} />
+          <circle cx={209.742} cy={51.496} r={5.072} />
+          <circle cx={226.648} cy={51.496} r={5.072} />
+          <circle cx={243.555} cy={51.496} r={5.072} />
+          <circle cx={260.462} cy={51.496} r={5.072} />
+          <circle cx={277.369} cy={51.496} r={5.072} />
+          <circle cx={294.275} cy={51.496} r={5.072} />
+          <circle cx={311.182} cy={51.496} r={5.072} />
+          <circle cx={6.86} cy={68.403} r={5.072} />
+          <circle cx={23.767} cy={68.403} r={5.072} />
+          <circle cx={40.674} cy={68.403} r={5.072} />
+          <circle cx={57.581} cy={68.403} r={5.072} />
+          <circle cx={74.487} cy={68.403} r={5.072} />
+          <circle cx={91.394} cy={68.403} r={5.072} />
+          <circle cx={108.301} cy={68.403} r={5.072} />
+          <circle cx={125.208} cy={68.403} r={5.072} />
+          <circle cx={142.114} cy={68.403} r={5.072} />
+          <circle cx={159.021} cy={68.403} r={5.072} />
+          <circle cx={175.928} cy={68.403} r={5.072} />
+          <circle cx={192.835} cy={68.403} r={5.072} />
+          <circle cx={209.742} cy={68.403} r={5.072} />
+          <circle cx={226.648} cy={68.403} r={5.072} />
+          <circle cx={243.555} cy={68.403} r={5.072} />
+          <circle cx={260.462} cy={68.403} r={5.072} />
+          <circle cx={277.369} cy={68.403} r={5.072} />
+          <circle cx={294.275} cy={68.403} r={5.072} />
+          <circle cx={311.182} cy={68.403} r={5.072} />
+        </g>
+        <g opacity={0.91} filter="url(#prefix__filter1_d_7_121)">
+          <path
+            d="M272.447-129.41C29.627-342.148-57.704-226.198-65.429-214.742c-.422.626-.567 1.28-.56 2.036l1.907 205.18c1.39 149.545 126.72 278.868 263.625 218.68 70.229-30.875 134.487-81.127 162.462-162.837 39.392-115.057-69.895-160.5-89.558-177.727z"
+            fill="#FF8A00"
+          />
+        </g>
+        <defs>
+          <filter
+            id="prefix__filter0_d_7_121"
+            x={-81.956}
+            y={-292.491}
+            width={549.293}
+            height={595.581}
+            filterUnits="userSpaceOnUse"
+            colorInterpolationFilters="sRGB"
+          >
+            <feFlood floodOpacity={0} result="BackgroundImageFix" />
+            <feColorMatrix
+              in="SourceAlpha"
+              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+              result="hardAlpha"
+            />
+            <feOffset dx={20.435} dy={20.435} />
+            <feGaussianBlur stdDeviation={28.201} />
+            <feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.04 0" />
+            <feBlend in2="BackgroundImageFix" result="effect1_dropShadow_7_121" />
+            <feBlend
+              in="SourceGraphic"
+              in2="effect1_dropShadow_7_121"
+              result="shape"
+            />
+          </filter>
+          <filter
+            id="prefix__filter1_d_7_121"
+            x={-101.956}
+            y={-292.491}
+            width={549.293}
+            height={595.581}
+            filterUnits="userSpaceOnUse"
+            colorInterpolationFilters="sRGB"
+          >
+            <feFlood floodOpacity={0} result="BackgroundImageFix" />
+            <feColorMatrix
+              in="SourceAlpha"
+              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+              result="hardAlpha"
+            />
+            <feOffset dx={20.435} dy={20.435} />
+            <feGaussianBlur stdDeviation={28.201} />
+            <feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.04 0" />
+            <feBlend in2="BackgroundImageFix" result="effect1_dropShadow_7_121" />
+            <feBlend
+              in="SourceGraphic"
+              in2="effect1_dropShadow_7_121"
+              result="shape"
+            />
+          </filter>
+        </defs>
+      </svg>
+    </Box>
+  )
+}
+
+export default TopOverlay
