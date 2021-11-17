a852632967d70c30f30bf76a46a26e47378b441d
aravindasiva <aravindcva@hotmail.com>
2021-11-17 23:40:59 +0000

    secrets, gif

--- pages/secret.tsx
+++ pages/secret.tsx
@@ -16,20 +16,21 @@ const Custom404 = () => {
             justifyContent='center'
             alignItems='center'
           >
-            <Image borderRadius='xl' src="cat-working.gif" alt="Working-cat" />
+            <Image borderRadius='xl' src="broken-heart.gif" alt="Working-cat" />
           </Box>
           <Box>
-            <Text textStyle={TextStyles.heading} fontSize={30} color='#FF8A00'>404 !! Cyber Cat is busy adding new page</Text>
+            <Text textStyle={TextStyles.heading} fontSize={20} color='#FF8A00'>You actually clicked it?</Text>
           </Box>
-          <Link p={4} href="/">
+            <Link p={4} href="/">
             <Button bg='#ff8a00'>Go Home</Button>
-          </Link>
+            </Link>
           <Box position='fixed' bottom={5}>
             <HStack>
               <Text textStyle={TextStyles.caption} fontSize={10} color='#d2d2d2'>Made with</Text>
               <Link href="/secret">
-                <Icon as={FaHeartBroken} _hover={{ color: 'red' }} _focus={{ outline: 'none' }} _active={{ outline: 'none' }} boxSize={3} color='#D2D2D2' />
-              </Link>              <Link href="https://github.com/aravindasiva" isExternal>
+              <Icon as={FaHeartBroken} _hover={{ color: 'red' }} _focus={{ outline: 'none' }} _active={{ outline: 'none' }} boxSize={3} color='#D2D2D2' />
+              </Link>
+              <Link href="https://github.com/aravindasiva" isExternal>
                 <Text textStyle={TextStyles.caption} fontSize={10} _hover={{ color: '#FF8A00' }} color='#d2d2d2'>Aravind Cva</Text>
               </Link>
             </HStack>
